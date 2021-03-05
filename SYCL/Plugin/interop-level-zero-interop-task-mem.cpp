14768f2b04c0f91e425951b7bb3953de1fd34627
Sergey V Maslov <sergey.v.maslov@intel.com>
2021-03-04 16:05:33 -0800

    clang-format
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

--- SYCL/Plugin/interop-level-zero-interop-task-mem.cpp
+++ SYCL/Plugin/interop-level-zero-interop-task-mem.cpp
@@ -12,27 +12,32 @@
 
 class my_selector : public cl::sycl::device_selector {
 public:
-    int operator()(const cl::sycl::device &dev) const override {
-        return (dev.get_platform().get_backend() == cl::sycl::backend::level_zero) ? 1 : 0;
-    }
+  int operator()(const cl::sycl::device &dev) const override {
+    return (dev.get_platform().get_backend() == cl::sycl::backend::level_zero)
+               ? 1
+               : 0;
+  }
 };
- 
+
 int main() {
-    sycl::queue sycl_queue = sycl::queue(my_selector());
- 
-    ze_context_handle_t ze_context = sycl_queue.get_context().get_native<sycl::backend::level_zero>();
-    std::cout << "zeContextGetStatus = " << zeContextGetStatus(ze_context) << std::endl;
- 
-    auto buf = cl::sycl::buffer<uint8_t, 1>(1024);
-    sycl_queue.submit([&](cl::sycl::handler &cgh) {
-        auto acc = buf.get_access<cl::sycl::access::mode::write>(cgh);
-        cgh.interop_task([&](const cl::sycl::interop_handler &ih) {
-            void *device_ptr = ih.get_mem<sycl::backend::level_zero>(acc);
-            ze_memory_allocation_properties_t memAllocProperties{};
-            zeMemGetAllocProperties(ze_context, device_ptr, &memAllocProperties, nullptr);
-            std::cout << "Memory type = " << memAllocProperties.type << std::endl;
-        });
+  sycl::queue sycl_queue = sycl::queue(my_selector());
+
+  ze_context_handle_t ze_context =
+      sycl_queue.get_context().get_native<sycl::backend::level_zero>();
+  std::cout << "zeContextGetStatus = " << zeContextGetStatus(ze_context)
+            << std::endl;
+
+  auto buf = cl::sycl::buffer<uint8_t, 1>(1024);
+  sycl_queue.submit([&](cl::sycl::handler &cgh) {
+    auto acc = buf.get_access<cl::sycl::access::mode::write>(cgh);
+    cgh.interop_task([&](const cl::sycl::interop_handler &ih) {
+      void *device_ptr = ih.get_mem<sycl::backend::level_zero>(acc);
+      ze_memory_allocation_properties_t memAllocProperties{};
+      zeMemGetAllocProperties(ze_context, device_ptr, &memAllocProperties,
+                              nullptr);
+      std::cout << "Memory type = " << memAllocProperties.type << std::endl;
     });
- 
-    return 0;
+  });
+
+  return 0;
 }
